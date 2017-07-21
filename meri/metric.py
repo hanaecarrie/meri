aa40a56360d632a1cb3c7217f2303bdc6c7d5517
hcherkaoui <hamza.cherkao@gmail.com>
2017-07-21 15:25:39 +0200

    [fix] fix error in metrics computations

--- meri/metric.py
+++ meri/metric.py
@@ -8,17 +8,23 @@ from skimage.measure import compare_ssim as _compare_ssim
 
 def _min_max_normalize(img):
     """ Center and normalize the given array.
+
     Parameters:
     ----------
     img: np.ndarray
+
+    Return:
+    ------
+    im: np.ndarray
+        the min-max normalized image.
     """
     min_img = img.min()
     max_img = img.max()
     return (img - min_img) / (max_img - min_img)
 
 
-def compute_ssim(test, ref):
-    """ Return ssim(ref, test)
+def compute_ssim(test, ref, mask="auto"):
+    """ Return SSIM
 
     Parameters:
     -----------
@@ -26,53 +32,47 @@ def compute_ssim(test, ref):
 
     test: np.ndarray, the tested image
 
+    mask: np.ndarray, the mask for the ROI
+
+    Notes:
+    ------
+    Compute the metric only on magnetude.
+
     Return:
     -------
     ssim: float, the snr
     """
-    #compute the ssim for all the image
     test = np.abs(test).astype('float64')
     ref = np.abs(ref).astype('float64')
     test = _min_max_normalize(test)
     ref = _min_max_normalize(ref)
-    _, ssim = _compare_ssim(test, ref, full=True)
-    # compute the mask for the foreground
-    centroids, mask, _ = k_means(ref.flatten()[:, None], 2)
-    if np.argmax(centroids) == 0:
-        mask = np.abs(mask-1)
-    mask = mask.reshape(*ref.shape)
-    mask = binary_closing(mask, np.ones((5, 5)), iterations=3).astype('int')
+    assim, ssim = _compare_ssim(test, ref, full=True)
+    if (not isinstance(mask, np.ndarray)) and (mask not in ["auto", None]):
+        raise ValueError("mask should be None, 'auto' or a np.ndarray,"
+                         " got '{0}' instead.".format(mask))
+    if mask is None:
+        return assim
+    if mask == "auto":
+        centroids, mask, _ = k_means(ref.flatten()[:, None], 2)
+        if np.argmax(centroids) == 0:
+            mask = np.abs(mask-1)
+        mask = mask.reshape(*ref.shape)
+        mask = binary_closing(mask, np.ones((5, 5)), iterations=4).astype('int')
     return (mask * ssim).sum() / mask.sum()
 
 
-def compute_assim(test, ref):
-    """ Return average ssim(ref, test)
-
-    Parameters:
-    -----------
-    ref: np.ndarray, the reference image
-
-    test: np.ndarray, the tested image
-
-    Return:
-    -------
-    ssim: float, the snr
-    """
-    test = np.abs(test).astype('float64')
-    ref = np.abs(ref).astype('float64')
-    test = _min_max_normalize(test)
-    ref = _min_max_normalize(ref)
-    return _compare_ssim(test, ref)
-
-
 def compute_snr(test, ref):
-    """ Return 10 log_10( |test|_2^2 /  |test - ref|_2^2)
+    """ Return SNR
 
     Parameters:
     -----------
     ref: np.ndarray, the reference image
 
     test: np.ndarray, the tested image
+
+    Notes:
+    ------
+    Compute the metric only on magnetude.
 
     Return:
     -------
@@ -80,13 +80,13 @@ def compute_snr(test, ref):
     """
     test = np.abs(test).astype('float64')
     ref = np.abs(ref).astype('float64')
-    test = _min_max_normalize(test)
-    ref = _min_max_normalize(ref)
-    return 10.0 * np.log10(np.linalg.norm(test)**2 / np.linalg.norm(test-ref)**2)
+    num = np.mean(np.square(ref))
+    deno = compute_mse(test, ref)
+    return 10.0 * np.log10(num / deno)
 
 
 def compute_psnr(test, ref):
-    """ Return 10 log_10( max(abs(test))^2 /  |test - ref|_2^2)
+    """ Return PSNR
 
     Parameters:
     -----------
@@ -104,13 +104,13 @@ def compute_psnr(test, ref):
     """
     test = np.abs(test).astype('float64')
     ref = np.abs(ref).astype('float64')
-    test = _min_max_normalize(test)
-    ref = _min_max_normalize(ref)
-    return 10.0 * np.log10(np.max(np.abs(test))**2 / np.linalg.norm(np.abs(test)-np.abs(ref))**2)
+    num = np.max(np.abs(ref))
+    deno = compute_mse(test, ref)
+    return 10.0 * np.log10(num / deno)
 
 
-def compute_nrmse(test, ref, norm_type="euclidian"):
-    """ Return |ref - test|_2 / norm(ref)
+def compute_mse(test, ref):
+    """ Return 1/N * |ref - test|_2
 
     Parameters:
     -----------
@@ -118,7 +118,31 @@ def compute_nrmse(test, ref, norm_type="euclidian"):
 
     test: np.ndarray, the tested image
 
-    norm_type: str, ('euclidian', 'min_max'), default='euclidian', the type of normalization
+    Notes:
+    -----
+    Compute the metric only on magnetude.
+
+    Return:
+    -------
+    mse: float, the mse
+    """
+    test = np.abs(test).astype('float64')
+    ref = np.abs(ref).astype('float64')
+    return np.mean(np.square(test - ref))
+
+
+def compute_nrmse(test, ref):
+    """ Return NRMSE
+
+    Parameters:
+    -----------
+    ref: np.ndarray, the reference image
+
+    test: np.ndarray, the tested image
+
+    Notes:
+    -----
+    Compute the metric only on magnetude.
 
     Return:
     -------
@@ -126,11 +150,6 @@ def compute_nrmse(test, ref, norm_type="euclidian"):
     """
     test = np.abs(test).astype('float64')
     ref = np.abs(ref).astype('float64')
-    test = _min_max_normalize(test)
-    ref = _min_max_normalize(ref)
-    if norm_type == "euclidian":
-        return np.linalg.norm(ref - test) / np.linalg.norm(ref)
-    elif norm_type == "min_max":
-        return np.linalg.norm(ref - test) / (np.max(ref) - np.min(ref))
-    else:
-        raise ValueError("'norm_type' not understood")
+    num = np.sqrt(compute_mse(test, ref))
+    deno = np.sqrt(np.mean((np.square(test))))
+    return num / deno
